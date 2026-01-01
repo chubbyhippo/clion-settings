277f4adddef3a9ecbab0328b7ab136a709a9a823
chubbyhippo <mkmsacc@outlook.com>
2026-01-02 01:10:20 +0700

    Update `install-plugins.sh` to improve CLion command resolution logic and add new plugins

--- install-plugins.sh
+++ install-plugins.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env sh
 
+# plugins
 cmd=clion
-
 if uname | grep -q "^MINGW"; then
-  cmd="$HOME"/AppData/Local/JetBrains/Toolbox/scripts/clion.cmd
+  if command -v clion.cmd >/dev/null 2>&1; then
+    cmd=clion.cmd
+  elif command -v clion64 >/dev/null 2>&1; then
+    cmd=clion.64
+  else
+    echo "Warning: neither clion.cmd nor clion64 found, falling back to clion"
+  fi
 fi
 
 $cmd installPlugins \
   IdeaVIM \
+  StringToolsPlugin \
+  com.fwdekker.randomness \
   com.github.camork.fileExpander \
+  com.joshestein.ideavim-quickscope \
   com.julienphalip.ideavim.peekaboo \
   com.joshestein.ideavim-quickscope \
-  eu.theblob42.idea.whichkey
+  eu.theblob42.idea.whichkey \
+  indent-rainbow.indent-rainbow \
+  izhangzhihao.rainbow.brackets.lite \
+  org.asciidoctor.intellij.asciidoc \
+  org.sonarlint.idea
